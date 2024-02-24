bb0e67910816cad46e1281fe4fb02f1f03de6c1c
Eugenio <eugenioclrc@gmail.com>
2024-02-24 12:12:31 -0300

    add emojis and responsivenss to links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,14 +11,16 @@ onMount(() => { initWeb3() });
 
 <header class="backdrop-blur-md border-b border-b-slate-800" style="background: #0b0d10">
   <div class="container flex justify-between py-2">
-    <hgroup class="m-0">
+    <hgroup class="pt-3">
       <h1 class="text-2xl"><a href="/">Buttplugs <small class="text-xs">(UwU)</small></a></h1>
-      <p><span class="text-sm">A Huff NFT collection</span></p>
+      <p class="hidden lg:inline"><span class="text-sm">A Huff NFT collection</span></p>
     </hgroup>
     <nav>
-      <ul>
-        <li><a href="https://opensea.io/collection/buttpluggy" target="_blank">OpenSea</a></li>
-        <li><a href="https://github.com/webtresclub/huffplug/tree/main/src" target="_blank">Source Code</a></li>
+      <ul class="text-xs lg:text-sm">
+        <li><a href="/mine"><span class="hidden lg:inline">🌱 </span>Mint</a></li>
+        <li class="hidden lg:inline"><a href="https://opensea.io/collection/buttpluggy" target="_blank">🌊 OpenSea</a></li>
+        <li class="hidden md:inline"><a href="https://github.com/webtresclub/huffplug/tree/main/src" target="_blank">
+          <span class="hidden lg:inline">💻 </span>Source Code</a></li>
         <li>{#if loadReady} <w3m-button /> {/if}</li>
       </ul>
     </nav>
@@ -29,3 +31,10 @@ onMount(() => { initWeb3() });
 <main class="container">
   <slot />
 </main>
+
+<style>
+  a:hover {
+    @apply underline text-slate-100;
+
+  }
+</style>
