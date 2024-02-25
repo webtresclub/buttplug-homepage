a8417629a52bad40af95e150fa316fa86a162247
Eugenio <eugenioclrc@gmail.com>
2024-02-25 00:01:19 -0300

    fix var name (is a store)

--- src/routes/mine/+page.svelte
+++ src/routes/mine/+page.svelte
@@ -110,7 +110,7 @@
 		{#if !$account}
 			<p>First connect your wallet</p>
 			<button
-				on:click={() => { modal.open() }}
+				on:click={() => { $modal.open() }}
 				class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
 			>Connect</button>
 		{:else}
