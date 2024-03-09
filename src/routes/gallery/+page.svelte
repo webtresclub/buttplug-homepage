cd6f35d992b6329f11bbf23505ab3640f1eeabf8
Tomas <tomasfreire@gmail.com>
2024-03-09 19:03:56 -0300

    [feat] gallery style

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -7,54 +7,61 @@
 	let nfts = [];
 	const ALCHEMY_API_KEY = 'KmDy5q8IZQLOWy8cKhEZB6tUcZgiAsRn';
 	const ALCHEMY_API_URL = `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
-  const buttpluggiesContractAddress = "0x0000420538CD5AbfBC7Db219B6A1d125f5892Ab0"
+	const buttpluggiesContractAddress = '0x0000420538CD5AbfBC7Db219B6A1d125f5892Ab0';
 
 	const web3 = new Web3(ALCHEMY_API_URL);
-		const settings = {
-			apiKey: ALCHEMY_API_KEY,
-			network: Network.ETH_MAINNET
-		};
+	const settings = {
+		apiKey: ALCHEMY_API_KEY,
+		network: Network.ETH_MAINNET
+	};
 
-		let options = {
-			excludeFilters: 'SPAM'
-		};
+	let options = {
+		excludeFilters: 'SPAM'
+	};
 
-		const alchemy = new Alchemy(settings);
+	const alchemy = new Alchemy(settings);
 
-		const getButtpluggies = async () => {
-			try {
-				// Get the NFTs owned by the wallet
-				let response = await alchemy.nft.getNftsForOwner($account, options);
+	const getButtpluggies = async () => {
+		try {
+			// Get the NFTs owned by the wallet
+			let response = await alchemy.nft.getNftsForOwner($account, options);
 
-				// Create a gallery of the NFTs
-				nfts = response.ownedNfts.filter((nft) => {
-					return nft.contract.address == buttpluggiesContractAddress;
-					// return ({
-					//   name: nft.name,
-					//   description: nft.description,
-					//   image: nft.image_url
-					// })
-				})
-        console.log({nfts})
-        return nfts
-			} catch (e) {
-				console.error({ e });
-			}
+			// Create a gallery of the NFTs
+			nfts = response.ownedNfts.filter((nft) => {
+				return nft.contract.address == buttpluggiesContractAddress;
+				// return ({
+				//   name: nft.name,
+				//   description: nft.description,
+				//   image: nft.image_url
+				// })
+			});
+			console.log({ nfts });
+			return nfts;
+		} catch (e) {
+			console.error({ e });
 		}
-  
-  $: if ($account && $loadReady) {
-    getButtpluggies()
-  }
+	};
 
-  onMount(async () => {});
+	$: if ($account && $loadReady) {
+		getButtpluggies();
+	}
 </script>
 
-<div class="gallery">
-	{#each nfts as nft (nft.name)}
-		<div class="nft">
-			<img width="276" height="276" src={nft.image.cachedUrl} alt={nft.name} />
-			<h2>{nft.name}</h2>
-			<p>{nft.description}</p>
-		</div>
-	{/each}
-</div>
+<main class="container">
+  <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+    {#each nfts as nft (nft.name)}
+      <div class="card mx-auto">
+        <div class="card__image">
+          <a href="" target="_blank">
+            <img width="276" height="276" src={nft.image.cachedUrl} alt={nft.name} />
+          </a>
+        </div>
+        <div class="card__content">
+          <h2>{nft.name}</h2>
+          <!-- <p>{nft.description}</p> -->
+        </div>
+      </div>
+    {/each}
+  </div>
+  
+</main>
