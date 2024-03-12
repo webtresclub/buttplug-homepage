67da8924c78b8388e9231e5c5c86006334f70c20
Eugenio <eugenioclrc@gmail.com>
2024-03-12 01:15:49 -0300

    remove proxy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
-const graph_url = 'https://api.studio.thegraph.com/proxy/67825/buttpluggy/v0.0.4/';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	server: {
-    proxy: {
-      '/api': {
-        target: graph_url,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 };
 
 export default config;
