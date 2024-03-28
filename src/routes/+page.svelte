07bd92a20ba50f8f11209865ea79e2d74137bed2
Eugenio <eugenioclrc@gmail.com>
2024-03-28 09:28:51 -0300

    Fix buttpluggy link url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -181,7 +181,7 @@
 
 		<div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
 			{#each lastIds as id}
-				<a href="/buttpluggy/{id}">
+				<a href="/buttpluggy/{id[0]}">
 					<div class="card mx-auto">
 						<div class="card__image">
 							<img
