5b442b521aa0b68591dc527f6ae04198b484d043
eugenioclrc <eugenioclrc@gmail.com>
2025-04-10 00:02:12 -0300

    missing files

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,44 @@
+<footer class="mt-0 bg-[#0b0d10] text-white/70 text-[0.84rem] tracking-[1px]">
+    <div class="container mx-auto px-4 py-8">
+        <div class="flex flex-col divide-y divide-white/10 space-y-4">
+            <div class="flex flex-col md:flex-row md:justify-between md:items-center gap-2 py-2">
+                <div class="text-white/30">SOCIALS</div>
+                <div class="flex flex-wrap gap-x-4">
+                    <a href="https://twitter.com/webtresclub" target="_blank">𝕏 / Twitter</a>
+                    <a href="https://github.com/webtresclub/huffplug" target="_blank">GitHub</a>
+                </div>
+            </div>
+
+            <div class="flex flex-col md:flex-row md:justify-between md:items-center gap-2 py-2">
+                <div class="text-white/30">PLATFORM</div>
+                <div class="flex flex-wrap gap-x-4">
+                    <a href="/mine">Mine</a>
+                    <a href="/claim">Claim</a>
+                    <a href="/attributes">Attributes</a>
+                </div>
+            </div>
+
+            <div class="flex flex-col md:flex-row md:justify-between md:items-center gap-2 py-2">
+                <div class="text-white/30">COLLECTION</div>
+                <div class="flex flex-wrap gap-x-4">
+                    <a href="https://opensea.io/collection/buttpluggy" target="_blank">OpenSea</a>
+                    <a href="/about">About</a>
+                    <a href="/faq">FAQ</a>
+                </div>
+            </div>
+
+            <div class="flex flex-col md:flex-row md:justify-between md:items-center gap-2 py-2">
+                <div class="text-white/30">LEGAL</div>
+                <div class="flex flex-wrap gap-x-4">Code licensed MIT, docs CC BY-SA 4.0.</div>
+            </div>
+        </div>
+
+        <div
+            class="mt-6 text-white/30 text-[0.7rem] flex flex-col md:flex-row md:justify-between md:items-center"
+        >
+            <span>Buttpluggy © {new Date().getFullYear()}</span>
+            <span class="hidden md:inline px-2">–</span>
+            <span class="mt-1 md:mt-0">A WebtrES Club Project</span>
+        </div>
+    </div>
+</footer>
